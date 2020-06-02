832895b5676d42561422db9c260d1658049d9fff
Nick Fletcher <nickfletcher720@gmail.com>
2020-06-02 15:36:11 +1000

    added more projects

--- src/Photography.js
+++ src/Photography.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import Fireworks from './assets/Fireworks.jpg';
-import JetPlaneClose from './assets/JetPlaneClose.jpg';
-import redFireworks from './assets/redFireworks.jpg';
-import SkateBoards from './assets/SkateBoards.jpg';
 import './styles/styles.scss'
 
 class Photography extends Component {
   render() {
     return (
       <div className="slider">
-        <div className="slide">
+        <span className="slide">
           <p>Slide 1</p>
-        </div>
-        <div className="slide">
+        </span>
+        <span className="slide">
           <p>Slide 2</p>
-        </div>
-        <div className="slide">
+        </span>
+        <span className="slide">
           <p>Slide 3</p>
-        </div>
-        <div className="slide">
+        </span>
+        <span className="slide">
           <p>Slide 4</p>
-        </div>
+        </span>
       </div>
     )
   }
--- src/Projects.js
+++ src/Projects.js
@@ -2,32 +2,48 @@ import React, { Component } from 'react';
 import pizzaApp from './assets/pizzaApp.jpg';
 import railsApp from './assets/railsApp.jpg';
 import terminalApp from './assets/terminalApp.jpg';
+import alphaBlog from './assets/alphaBlog.jpg';
+import financeTracker from './assets/financeTracker.jpg';
 import './styles/styles.scss'
 
 class Projects extends Component {
   render() {
     return(
-      <div className="projects">
-        <div>
-          <img src={pizzaApp}  fluid rounded />
-          <a target="_blank" href="https://github.com/nickf94/PizzaApp">
+    <div className="projects">
+      <div>
+        <img src={pizzaApp}  fluid rounded />
+        <a target="_blank" href="https://github.com/nickf94/PizzaApp">
           <button className="projects-btn">PizzaApp</button>
-          </a>
-        </div>
-
-        <div>
-          <img src={railsApp} fluid rounded />
-          <a target="_blank" href="https://github.com/nickf94/Rails-Marketplace-App">
-          <button className="projects-btn">RailsApp</button>
-          </a>
-        </div>
-
-        <div>
-          <img src={terminalApp} fluid rounded />
-          <a target="_blank" href="https://github.com/nickf94/Question-and-Answers">
-          <button className="projects-btn">TerminalApp</button>
-          </a>
+        </a>
       </div>
+
+      <div>
+        <img src={railsApp} fluid rounded />
+        <a target="_blank" href="https://rails-marketplace-app.herokuapp.com/">
+          <button className="projects-btn">RailsApp</button>
+        </a>
+      </div>
+
+      <div>
+        <img src={terminalApp} fluid rounded />
+        <a target="_blank" href="https://github.com/nickf94/Question-and-Answers">
+          <button className="projects-btn">TerminalApp</button>
+        </a>
+      </div>
+
+        <div>
+          <img src={alphaBlog} fluid rounded />
+          <a target="_blank" href="https://alpha-blog1994.herokuapp.com/">
+            <button className="projects-btn">AlphaBlog</button>
+          </a>
+        </div>
+
+        <div>
+          <img src={financeTracker} fluid rounded />
+          <a target="_blank" href="https://nicks-finance-tracker.herokuapp.com/">
+            <button className="projects-btn">Finance Tracker App</button>
+          </a>
+        </div>
       </div>
     )
   }
